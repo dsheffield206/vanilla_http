37f2eceef4fdd15d8a9969e47da3a00e25f65202
David Sheffield <david.sheffield21@gmail.com>
2015-09-08 19:39:38 -0700

    updating http_server.js for homework

--- http_server.js
+++ http_server.js
@@ -1,15 +1,38 @@
 'use strict';
 
-var net = require('net');
+var http = require('http');
+var server = net.createServer(function(req, res) {
+    debugger;
+    if(req.url === '/') {
+        res.writeHead(200, {
+            'Content-Type': 'text/plain'
+        });
+        res.write('hello from my server!');
+        return res.end();
+    }
 
-var server = net.createServer(function(socket) {
-    socket.on('data', function(data) {
-        console.log(data.toString());
+    if (req.method === 'POST') {
+        req.on('data', function(data) {
+            var parsed = JSON.parse(data.toString());
+            console.log('parsing ' + data.toString());
+        });
+
+        res.writeHead(200, {
+            'Content-Type': 'application/json'
+        });
+
+        res.write('{"msg": "success!"}');
+        return res.end();
+    }
+
+    res.writeHead(404, {
+        'Content-Type': 'text/plain'
     });
 
-    socket.on('end', function() {
-        console.log('discounnected');
-    });
+    res.write('page not found');
+    res.end();
 });
 
-server.listen(3000);
+server.listen(3000, function() {
+    console.log('server up');
+});
